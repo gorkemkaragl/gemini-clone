b684316616b58dea898f93d04ae2b1032f95a787
gorkemkaragl <gorkemkaragol0@gmail.com>
2025-10-03 15:42:26 +0300

    feat: Google AI integration & Main styles

---
+++ src/config/gemini.js
@@ -0,0 +1,36 @@
+const apiKey="AIzaSyAU91SvwQYhdt7AFZXxZPFw6zoF-AgxeF0";
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  }  from "@google/generative-ai";
+  
+
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function runChat(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+  export default runChat;
