22d44a89e8bccd5bbe89e996a9a374e1abf80151
gorkemkaragl <gorkemkaragol0@gmail.com>
2025-10-05 22:36:21 +0300

    feat: enhance Main with context and style updates

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,19 +1,80 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 export const Context =createContext();
 
 const ContextProvider =(props)=>{
 
-    const onSent=async(prompt)=>{
-        await runChat(prompt);
+    const [input,setInput]=useState("");
+    const [recentPrompt,setRecentPrompt]=useState("");
+    const [prevPrompts,setPrevPrompts]=useState([]);
+    const [showResult,setShowResult]=useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("")
+
+    const delayPara=(i,word)=>{
+        setTimeout(()=>{
+            setResultData(prev=>prev+word);
+        },i*10);
     }
 
-    onSent("hello?");
+    const onSent=async (prompt)=>{
+        setResultData("");
+        setLoading(true);
+        setShowResult(true);
+        let response;
+        if(prompt!==undefined){
+            response = await runChat(prompt);
+            setRecentPrompt(prompt)
+        }
+        else{
+            setPrevPrompts(prev=>[...prev,input]);
+            setRecentPrompt(input);
+            response=await runChat(input);
+        }
+        try{
+            let responseArray = response.split("**");
+            let newResponse = "";
+            for(let i=0;i<responseArray.length;i++){
+                if(i==0||i%2!==1){ //even indexes
+                    newResponse += responseArray[i];
+                }
+                else{
+                    newResponse += "<b>" + responseArray[i] + "</b>";
+                }
+            }
+            let newResponse2= newResponse.split('*').join('<br/>');
+            let newResponseArray = newResponse2.split("");
+            for (let i = 0; i < newResponseArray.length; i++) {
+                const nextWord = newResponseArray[i];
+                delayPara(i, nextWord + "");
+            }
+        }
+        catch(error){
+            console.log(error);
+        }
+        finally {
+            setLoading(false);
+            setInput("");
+        }
+    }
+
+
 
     const contextValue={
-
-    };
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        input,
+        setInput,
+        recentPrompt,
+        setRecentPrompt,
+        showResult,
+        resultData,
+        setShowResult,
+        loading,
+        setLoading,
+    }
 
     return(
         <Context.Provider value={contextValue}>
